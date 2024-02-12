fcc034904aab934972cc60afa9d914e4d40ff6fd
Eva Björling <eva.bjorling@chasacademy.se>
2024-02-12 15:16:44 +0100

    custom events now added - add your own in the file

---
+++ js/custom-events-ga4.js
@@ -0,0 +1,55 @@
+"use strict";
+// funktion som spårar sökfunktionen
+function trackSearchLinkClick() {
+    // skickar event till GA4
+    window.gtag('event', 'search_link_clicked', {
+        'event_category': 'Navigation',
+        'event_label': 'Search Link Clicked'
+    });
+}
+/*** MALL FÖR FUNCTION
+ * function NAMNET PÅ DIN FUNKTION() {
+    // skickar event till GA4
+    // BYT UT ALLA NAMN
+    (window as any).gtag('event', 'search_link_clicked', {
+        'event_category': 'Navigation',
+        'event_label': 'Search Link Clicked'
+    });
+}
+ */
+// undersöker att g-tagen har laddats in innan våra spårningar börjar
+window.onload = function () {
+    // undersöker att g-tagen är tillgänglig
+    if (typeof window.gtag === 'function') {
+        // definera/deklarera elementet att spåra
+        const searchLink = document.getElementById('search-link');
+        //om det finns - länka en eventListener
+        if (searchLink) {
+            searchLink.addEventListener('click', trackSearchLinkClick);
+        }
+        /******MALL FÖR CUSTOM EVENT******* */
+        // if (typeof (window as any).gtag === 'function') {
+        //     const DITT-ELEMENT = document.getElementById('ID PÅ DITT ELEMENT');
+        //     if (DITT ELEMENT) {
+        //         DITT ELEMENT.addEventListener('click', DIN FUNKTION);
+        //     }
+        // }
+    }
+    else {
+        // om gtag inte hittas - letar den igen efter en delay
+        setTimeout(function () {
+            if (typeof window.gtag === 'function') {
+                const searchLink = document.getElementById('search-link');
+                if (searchLink) {
+                    searchLink.addEventListener('click', trackSearchLinkClick);
+                }
+            }
+            // if (typeof (window as any).gtag === 'function') {
+            //     const DITT-ELEMENT = document.getElementById('ID PÅ DITT ELEMENT');
+            //     if (DITT ELEMENT) {
+            //         DITT ELEMENT.addEventListener('click', DIN FUNKTION);
+            //     }
+            // }
+        }, 1000); // Adjust the delay time as needed
+    }
+};
